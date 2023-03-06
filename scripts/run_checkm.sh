572fc61212ad30aa967127765c9d7aa10944322e
Mani Arumugam <mjq180@ku.dk>
2023-03-06 14:59:07 +0100

    Removed obsolete scripts for MAG generation

--- scripts/run_checkm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-checkm_threads=$1
-pplacer_threads=$2
-
-
-for folder in $(ls .);
-do
-checkm lineage_wf ${folder} checkm${folder} -x fna --threads ${checkm_threads} --pplacer_threads ${pplacer_threads} --tab_table -f checkm${folder}.tsv
-done
