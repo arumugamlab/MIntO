1b31c019c4c71aef280caa01d26b7f9b482cfde6
Mani Arumugam <mjq180@ku.dk>
2023-06-07 09:23:55 +0200

    Removed obsolete code parts

--- scripts/MAGs_genes_out_subset.R
+++ scripts/MAGs_genes_out_subset.R
@@ -16,18 +16,11 @@ dir_out <- args[5]
 file_name <- args[6]
 
 ##########################  ** Load directories **  ########################## 
-# threads_n <- 8
-# bed_file <- "/emc/cbmr/users/rzv923/ibdmdb/DB/9-MAGs-prokka-post-analysis/GFF/MAGs_genes.bed"
-# cd_transl_file <- "/emc/cbmr/users/rzv923/ibdmdb/DB/9-MAGs-prokka-post-analysis/CD_transl/MAGs_genes_translated_cds.faa"
-# dir_out <- "/emc/cbmr/users/rzv923/ibdmdb/DB/9-MAGs-prokka-post-analysis/"
-# file_name <- "MAGs_genes"
-# normalization <- "MG"
-# path_out <- "/emc/cbmr/users/rzv923/ibdmdb/DB/9-MAGs-prokka-post-analysis/"
 
 setDTthreads(threads = threads_n)
+
 ##########################  ** Read file **  ########################## 
 # Prokka genes - bed file ####
-#bed_file <- '/emc/cbmr/users/rzv923/ibdmdb/DB/9-MAGs-post-analysis-prokka/predicted_genes.bed'
 
 bed_info <- c("chr","start","stop","name","score","strand","source","feature","frame","info")
 genomes_df = read.csv(bed_file, sep ="\t", header = F, stringsAsFactors = F)
@@ -142,39 +135,3 @@ fastafile_genes_sub <- fastafile_genes[names(fastafile_genes) %in% genomes_sub_d
 fastafile_sub <- c(fastafile_genes_sub)
 
 writeXStringSet(fastafile_sub , paste0(dir_out, file_name, "_translated_cds_SUBSET.faa"))
-
-
-# # remove duplicated headers from fasta file
-# # bash
-# cd /emc/cbmr/data/microbiome/processed/SHIME/Diet1/DB/9-MAGs-post-analysis-prokka/CD_transl
-# awk '/^>/ { f = !($0 in a); a[$0] } f==1 { print $0 }' genes-PBGC-SBGC-smORFs_SUBSET_translated_cds.faa > genes-PBGC-SBGC-smORFs_SUBSET_translated_cds_no-dupl.faa
-
-# if (normalization == 'MG'){
-#   # Before running fetchMG (to identify marker genes in genomes)
-#   ## From CDS tranls fasta file with ALL-SUBSET sequences, separate by genome ####
-#   inFasta <- readAAStringSet(paste0(dir_out, file_name, "_translated_cds_SUBSET.faa"))
-#   
-#   name_list <- sort(list.files(paste0(dir_out, 'fasta/')))
-#   name_list <- name_list[name_list %like% ".fna"]
-#   
-#   for (cds_file in (1:length(name_list))){
-#     cds_file_name <- name_list[cds_file]
-#     cds_file_genome <- gsub('.fna', '', cds_file_name)
-#     ## subset fasta file by matching seq names from fasta and genome ID
-#     inFasta_sub <- inFasta[names(inFasta) %like% cds_file_genome]
-#     
-#     ## Modify headers - replace '.' by '-'
-#     fa_given_names <- names(inFasta_sub)
-#     ## prepare data frame
-#     inFasta_sub_df <- data.frame(names(inFasta_sub))
-#     names(inFasta_sub_df) <- c('header')
-#     inFasta_sub_df$header_repl <- gsub('\\.', '\\-', inFasta_sub_df$header)
-#     inFasta_sub_df <- subset(inFasta_sub_df, select=c('header', 'header_repl'))
-#     
-#     ## assign new seq names by mapping fasta seq name to data frame names
-#     names(inFasta_sub) <- inFasta_sub_df[match(fa_given_names , inFasta_sub_df$header) , "header_repl"]
-#     
-#     writeXStringSet(inFasta_sub , paste0(dir_out, "CD_transl/",cds_file_genome, '_translated_cds_SUBSET.faa'))
-#   }
-#   cat(paste0("Ready to run FetchMGs"),file=paste0(out_dir, "CD_transl/", file_name, "_fetchMGs_translated_cds_SUBSET.txt"),sep="\n")
-# }
