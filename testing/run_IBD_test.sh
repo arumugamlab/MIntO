350625c8a728e5b7af8762a746713cd19bb6b97b
Mani Arumugam <mjq180@ku.dk>
2023-06-13 12:38:37 +0200

    Forgot to cd back after 'git pull'

--- testing/run_IBD_test.sh
+++ testing/run_IBD_test.sh
@@ -11,6 +11,7 @@ SNAKE_PARAMS="--use-conda --restart-times 1 --keep-going --latency-wait 60 --con
 if [ -d "MIntO" ]; then
   cd MIntO
   git pull
+  cd ..
 else
   git clone https://github.com/arumugamlab/MIntO.git
 fi
