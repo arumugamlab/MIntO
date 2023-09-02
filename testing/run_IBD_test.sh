61eed20124ace79ae5fe66d874509f926932f6dd
Mani Arumugam <mjq180@ku.dk>
2023-09-02 21:06:54 +0200

    Added CODE_DIR to specify where Snakemake scripts are located

--- testing/run_IBD_test.sh
+++ testing/run_IBD_test.sh
@@ -43,8 +43,6 @@ fi
 
 COMMAND_LOG='commands.txt'
 
-# Download dependencies
-
 # Snakemake options
 if [ ! -z "$COMPUTEROME_PROJ" ]; then
   SNAKE_PARAMS="--use-conda --restart-times 1 --keep-going --latency-wait 60 --conda-prefix $CONDA_DIR --shadow-prefix $SHADOWDIR --jobs 16 --default-resources gpu=0 mem=4 --cluster 'qsub -d $(pwd) -W group_list=$COMPUTEROME_PROJ -A $COMPUTEROME_PROJ -N {name} -l nodes=1:thinnode:ppn={threads},mem={resources.mem}gb,walltime=7200 -V -v TMPDIR=$SHADOWDIR' --local-cores 4"
@@ -55,9 +53,14 @@ else
   SNAKE_PARAMS="--use-conda --restart-times 1 --keep-going --latency-wait 60 --conda-prefix $CONDA_DIR --shadow-prefix $SHADOWDIR --jobs 16 --cores 96 --resources mem=700"
 fi
 
+# Set code directory
+CODE_DIR=$MINTO_DIR
+
+# Download dependencies
+
 echo -n "Downloading dependencies: "
 cat $MINTO_DIR/testing/dependencies.yaml.in | sed "s@<__MINTO_DIR__>@$MINTO_DIR@;s@<__TEST_DIR__>@$TEST_DIR@" > dependencies.yaml
-cmd="snakemake --snakefile $MINTO_DIR/smk/dependencies.smk --configfile dependencies.yaml $SNAKE_PARAMS >& dependencies.log"
+cmd="snakemake --snakefile $CODE_DIR/smk/dependencies.smk --configfile dependencies.yaml $SNAKE_PARAMS >& dependencies.log"
 echo $cmd > $COMMAND_LOG
 time (eval $cmd && echo "OK")
 
@@ -93,53 +96,53 @@ for OMICS in metaG metaT; do
 
   echo -n "QC_1: "
   cat $MINTO_DIR/testing/QC_1.yaml.in | sed "s@<__MINTO_DIR__>@$MINTO_DIR@;s@<__TEST_DIR__>@$TEST_DIR@;s@<__OMICS__>@$OMICS@;" > QC_1.yaml
-  cmd="snakemake --snakefile $MINTO_DIR/smk/QC_1.smk --configfile QC_1.yaml $SNAKE_PARAMS >& QC_1.log"
+  cmd="snakemake --snakefile $CODE_DIR/smk/QC_1.smk --configfile QC_1.yaml $SNAKE_PARAMS >& QC_1.log"
   echo $cmd >> $COMMAND_LOG
   time (eval $cmd && echo "OK")
 
   if [ ! -f "QC_2.yaml.fixed" ]; then
-    patch QC_2.yaml $MINTO_DIR/testing/QC_2.patch -o - | sed "s@<__MINTO_DIR__>@$MINTO_DIR@;s@<__TEST_DIR__>@$TEST_DIR@" > QC_2.yaml.fixed
+    patch QC_2.yaml $CODE_DIR/testing/QC_2.patch -o - | sed "s@<__MINTO_DIR__>@$MINTO_DIR@;s@<__TEST_DIR__>@$TEST_DIR@" > QC_2.yaml.fixed
   fi
 
   echo -n "QC_2: "
-  cmd="snakemake --snakefile $MINTO_DIR/smk/QC_2.smk --configfile QC_2.yaml.fixed $SNAKE_PARAMS >& QC_2.log"
+  cmd="snakemake --snakefile $CODE_DIR/smk/QC_2.smk --configfile QC_2.yaml.fixed $SNAKE_PARAMS >& QC_2.log"
   echo $cmd >> $COMMAND_LOG
   time (eval $cmd && echo "OK")
 
   echo -n "ASSEMBLY: "
   sed "s@enable_COASSEMBLY: no@enable_COASSEMBLY: yes@;" assembly.yaml > assembly.yaml.fixed
-  cmd="snakemake --snakefile $MINTO_DIR/smk/assembly.smk --configfile assembly.yaml.fixed $SNAKE_PARAMS >& assembly.log"
+  cmd="snakemake --snakefile $CODE_DIR/smk/assembly.smk --configfile assembly.yaml.fixed $SNAKE_PARAMS >& assembly.log"
   echo $cmd >> $COMMAND_LOG
   time (eval $cmd && echo "OK")
 
   echo -n "BINNING_PREP: "
-  cmd="snakemake --snakefile $MINTO_DIR/smk/binning_preparation.smk --configfile assembly.yaml.fixed $SNAKE_PARAMS >& binning_prep.log"
+  cmd="snakemake --snakefile $CODE_DIR/smk/binning_preparation.smk --configfile assembly.yaml.fixed $SNAKE_PARAMS >& binning_prep.log"
   echo $cmd >> $COMMAND_LOG
   time (eval $cmd && echo "OK")
 
   echo -n "BINNING: "
-  cmd="snakemake --snakefile $MINTO_DIR/smk/mags_generation.smk --configfile mags_generation.yaml $SNAKE_PARAMS >& mags.log"
+  cmd="snakemake --snakefile $CODE_DIR/smk/mags_generation.smk --configfile mags_generation.yaml $SNAKE_PARAMS >& mags.log"
   echo $cmd >> $COMMAND_LOG
   time (eval $cmd && echo "OK")
 
   echo -n "GENE_ANNOTATION - MAG: "
-  cmd="snakemake --snakefile $MINTO_DIR/smk/gene_annotation.smk --configfile mapping.yaml $SNAKE_PARAMS >& annotation.log"
+  cmd="snakemake --snakefile $CODE_DIR/smk/gene_annotation.smk --configfile mapping.yaml $SNAKE_PARAMS >& annotation.log"
   echo $cmd >> $COMMAND_LOG
   time (eval $cmd && echo "OK")
 
   echo -n "GENE_ABUNDANCE - MAG: "
-  cmd="snakemake --snakefile $MINTO_DIR/smk/gene_abundance.smk --configfile mapping.yaml $SNAKE_PARAMS >& abundance.log"
+  cmd="snakemake --snakefile $CODE_DIR/smk/gene_abundance.smk --configfile mapping.yaml $SNAKE_PARAMS >& abundance.log"
   echo $cmd >> $COMMAND_LOG
   time (eval $cmd && echo "OK")
 
   sed "s@map_reference: MAG@map_reference: reference_genome@; s@PATH_reference:@PATH_reference: $TEST_DIR/genomes@;" mapping.yaml > mapping.yaml.refgenome
   echo -n "GENE_ANNOTATION - refgenome: "
-  cmd="snakemake --snakefile $MINTO_DIR/smk/gene_annotation.smk --configfile mapping.yaml.refgenome $SNAKE_PARAMS >& annotation.refgenome.log"
+  cmd="snakemake --snakefile $CODE_DIR/smk/gene_annotation.smk --configfile mapping.yaml.refgenome $SNAKE_PARAMS >& annotation.refgenome.log"
   echo $cmd >> $COMMAND_LOG
   time (eval $cmd && echo "OK")
 
   echo -n "GENE_ABUNDANCE - refgenome: "
-  cmd="snakemake --snakefile $MINTO_DIR/smk/gene_abundance.smk --configfile mapping.yaml.refgenome $SNAKE_PARAMS >& abundance.refgenome.log"
+  cmd="snakemake --snakefile $CODE_DIR/smk/gene_abundance.smk --configfile mapping.yaml.refgenome $SNAKE_PARAMS >& abundance.refgenome.log"
   echo $cmd >> $COMMAND_LOG
   time (eval $cmd && echo "OK")
 
@@ -149,22 +152,22 @@ done
 # Run integration
 
 echo -n "DATA_INTEGRATION - MAG, MG: "
-cmd="snakemake --snakefile $MINTO_DIR/smk/data_integration.smk --configfile data_integration.yaml $SNAKE_PARAMS >& integration.MAG.MG.metaGT.log"
+cmd="snakemake --snakefile $CODE_DIR/smk/data_integration.smk --configfile data_integration.yaml $SNAKE_PARAMS >& integration.MAG.MG.metaGT.log"
 echo $cmd >> $COMMAND_LOG
 time (eval $cmd && echo "OK")
 sed "s/abundance_normalization: MG/abundance_normalization: TPM/" data_integration.yaml > data_integration.yaml.TPM
 echo -n "DATA_INTEGRATION - MAG, TPM: "
-cmd="snakemake --snakefile $MINTO_DIR/smk/data_integration.smk --configfile data_integration.yaml.TPM $SNAKE_PARAMS >& integration.MAG.TPM.metaGT.log"
+cmd="snakemake --snakefile $CODE_DIR/smk/data_integration.smk --configfile data_integration.yaml.TPM $SNAKE_PARAMS >& integration.MAG.TPM.metaGT.log"
 echo $cmd >> $COMMAND_LOG
 time (eval $cmd && echo "OK")
 
 echo -n "DATA_INTEGRATION - refgenome, MG: "
 sed "s/map_reference: MAG/map_reference: reference_genome/" data_integration.yaml > data_integration.yaml.refgenome
-cmd="snakemake --snakefile $MINTO_DIR/smk/data_integration.smk --configfile data_integration.yaml.refgenome $SNAKE_PARAMS >& integration.refgenome.MG.metaGT.log"
+cmd="snakemake --snakefile $CODE_DIR/smk/data_integration.smk --configfile data_integration.yaml.refgenome $SNAKE_PARAMS >& integration.refgenome.MG.metaGT.log"
 echo $cmd >> $COMMAND_LOG
 time (eval $cmd && echo "OK")
 sed "s/abundance_normalization: MG/abundance_normalization: TPM/" data_integration.yaml.refgenome > data_integration.yaml.refgenome.TPM
 echo -n "DATA_INTEGRATION - refgenome, TPM: "
-cmd="snakemake --snakefile $MINTO_DIR/smk/data_integration.smk --configfile data_integration.yaml.refgenome.TPM $SNAKE_PARAMS >& integration.refgenome.TPM.metaGT.log"
+cmd="snakemake --snakefile $CODE_DIR/smk/data_integration.smk --configfile data_integration.yaml.refgenome.TPM $SNAKE_PARAMS >& integration.refgenome.TPM.metaGT.log"
 echo $cmd >> $COMMAND_LOG
 time (eval $cmd && echo "OK")
