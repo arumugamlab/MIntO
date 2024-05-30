f5f122ae56b5cf4ff32b934976ee107b6de016c8
Mani Arumugam <mjq180@ku.dk>
2024-05-30 09:35:34 +0200

    Explicit use of output file names; preserve strand information in genes.

--- scripts/MAGs_genes_out_subset.R
+++ scripts/MAGs_genes_out_subset.R
@@ -6,22 +6,23 @@ library(tidyr)
 library(Biostrings)
 library(stringr)
 
-bed_file <- args[1]
-cd_transl_file <- args[2]
-dir_out <- args[3]
-file_name <- args[4]
+in_bed_file <- args[1]
+in_faa_file <- args[2]
+
+out_bed_file <- args[3]
+out_faa_file <- args[4]
 
 ##########################  ** Read file **  ########################## 
 # Prokka genes - bed file ####
 
 bed_info <- c("chr","start","stop","name","score","strand","source","feature","frame","info")
-genomes_df = read.csv(bed_file, sep ="\t", header = F, stringsAsFactors = F)
+genomes_df = read.csv(in_bed_file, sep ="\t", header = F, stringsAsFactors = F)
 names(genomes_df) <- bed_info
 
 # Group duplicated features
-genomes_df$coord <- paste0(genomes_df$chr, '_',genomes_df$start, '_', genomes_df$stop)
+genomes_df$coord <- paste0(genomes_df$chr, '_', genomes_df$start, '_', genomes_df$stop)
 
-genomes_sub <- subset(genomes_df, select=c('coord', 'name', 'source', 'feature'))
+genomes_sub <- subset(genomes_df, select=c('coord', 'name', 'source', 'feature', 'strand'))
 
 # rank feature sources
 source_list <- unique(genomes_sub$source)
@@ -37,6 +38,7 @@ genomes_sub_dplyr <- as.data.frame(genomes_sub %>%
                                      dplyr::group_by(coord)%>%
                                      dplyr::summarise(name = paste(unique(name), collapse=';'),
                                                       source = paste(unique(source), collapse=','),
+                                                      strand = paste(unique(strand), collapse=','),
                                                       feature = paste(unique(feature), collapse='|')))
 
 genomes_sub2 <- subset(genomes_df, select=c('coord','chr', 'start', 'stop'))
@@ -47,7 +49,6 @@ genomes_sub_df <- merge(genomes_sub2, genomes_sub_dplyr, by='coord')
 
 genomes_sub_df$score <- '.'
 genomes_sub_df$frame <- '.'
-genomes_sub_df$strand <- '.'
 
 #genomes_sub_df$source2 <- unlist(lapply(strsplit(as.character(genomes_sub_df$source),'\\,'), `[`, 1))
 genomes_sub_df$genome <- unlist(lapply(strsplit(as.character(genomes_sub_df$chr),'\\|'), `[`, 1))
@@ -61,15 +62,15 @@ genomes_sub_df <- as.data.frame(genomes_sub_df %>%
                                   dplyr::select(chr,start,stop,name,score,strand,source,feature,frame,info)) %>%
                                   dplyr::arrange(chr, start, stop)
 
-write.table(genomes_sub_df,paste0(dir_out, file_name, "_SUBSET.bed"),sep="\t",row.names=FALSE, col.names = F, quote = F)
+write.table(genomes_sub_df, out_bed_file, sep="\t", row.names=FALSE, col.names = F, quote = F)
 
 # delete from use
 rm(genomes_df, genomes_sub, genomes_sub_dplyr, genomes_sub2)
 
 
-#### #### ####  CD_transl file #### #### ####
+#### #### ####  Protein fasta file #### #### ####
 ## Load CDs transl sequences
-fastafile_MAGgenes<- readAAStringSet(file = cd_transl_file)
+fastafile_MAGgenes<- readAAStringSet(file = in_faa_file)
 
 ## prepare data frame
 fastafile_MAGgenes_df <- data.frame(names(fastafile_MAGgenes))
@@ -82,4 +83,4 @@ names(fastafile_MAGgenes) <- fastafile_MAGgenes_df$ID
 # subselect features in BED
 fastafile_genes_sub <- fastafile_MAGgenes[names(fastafile_MAGgenes) %in% genomes_sub_df$info]
 
-writeXStringSet(fastafile_genes_sub , paste0(dir_out, file_name, "_translated_cds_SUBSET.faa"))
+writeXStringSet(fastafile_genes_sub , out_faa_file)
