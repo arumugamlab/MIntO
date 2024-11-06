7c24338e85fc20fbd975355b83f065795224a26d
Mani Arumugam <mjq180@ku.dk>
2024-11-06 15:35:49 +0100

    Added a function to report memory usage for a list of variables.

--- scripts/include/utils.R
+++ scripts/include/utils.R
@@ -8,3 +8,25 @@
 logmsg <- function(...) {
     message("# ", format(Sys.time(), digits=0), " - ", paste(list(...)), collapse=" ")
 }
+
+# Return the mem usage (in bytes) of a list of variables in a given environment (calling env by default)
+# Combine into a data frame for better readability
+report_mem_usage <- function(variables, env = parent.frame()) {
+    library(pryr)
+
+    # Is this variable of interest?
+    var_of_interest <- function(x, env) {
+        voi = get(x, envir = env)
+        !(is.function(voi) || (is.atomic(voi) && length(voi)<=1))
+    }
+
+    # Get variables of interest: skip functions and scalars
+    v = variables[sapply(variables, var_of_interest, env)]
+
+    # Get a df with their sizes
+    df = data.frame(VARIABLE = v, SIZE = sapply(v, function(x) pryr::object_size(get(x, env))))
+    rownames(df) = NULL
+
+    # Pad with hlines for readability
+    return(rbind(data.frame(VARIABLE = "--------", SIZE = "--------"), df, data.frame(VARIABLE = c("--------", "TOTAL", "--------"), SIZE = c("--------", sum(df$SIZE), "--------"))))
+}
